3de33e13fac125cd4dc596e8f49535f6adb44630
Daniel <danielbankole33@yahoo.com>
2022-10-25 11:54:39 +0100

    added more information to connect to network

--- elonNFT/hardhat.config.js
+++ elonNFT/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config(); //all the key value pairs are being made available due to this lib
+
+const {API_URL_KEY, PRIVATE_KEY} = process.env; //environment variables are loaded here
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: API_URL_KEY,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
